d0ac79593eeef505fc8c847ef308e7c4396868cc
Tedu <1824687749@qq.com>
2020-11-18 14:57:05 +0800

    初始化提交

---
+++ settings/develop.py
@@ -0,0 +1,10 @@
+from .base import *  #NOQA
+
+#开发环境使用
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
